0978d14164885ada94c8056fe5b4a60a00cfab3a
joedispenza <140435546+joedispenza@users.noreply.github.com>
2023-07-25 18:06:10 +0200

    implementation de l'enregistrement de users
    Chaque utilisateur peut s'enregistrer. Les validations d'email, de mot passe etc sont implementer

--- api-restau-app/routes/api.php
+++ api-restau-app/routes/api.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function() {
     Route::apiResource('commande', CommandeController::class);
 
